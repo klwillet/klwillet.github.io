03a76a96ea10fe7ce0f9a5c425dc3384fc6af8a8
klwillet <klwillet@svsu.edu>
2026-03-24 16:17:27 -0400

    Add files via upload

---
+++ 355Assignment2/index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/"){
+        fs.readFile(path.join(__dirname, "index.html"),
+        (err, content)=>{
+            if(err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end(content)
+        });
+    }
+    else if (req.url === "/api"){
+        fs.readFile(path.join(__dirname, "db.json"), "utf8", 
+        (err, content)=>{
+                if(err) throw err;
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.end(content)
+        });
+    } 
+    else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Not Found");
+    }
+});
+
+server.listen(222, () => {
+    console.log(`Server running on port 222`);
+});
